c245853df9f527e974c2c6599303d10e80ff5280
Katelyn Gadd <kg@luminance.org>
2015-11-17 15:55:58 -0800

    Checkpoint

---
+++ test/testUtil.ts
@@ -0,0 +1,31 @@
+type CallRecord = [string, Array<any>];
+
+class MockHandlerProxyHandler {
+  log : Array<CallRecord>;
+
+  constructor (log: Array<CallRecord>) {
+    this.log = log;
+  }
+
+  get (state, propertyName : string, receiver) {
+    var self = this;
+    return function mockedMethod () {
+      self.log.push([propertyName, Array.prototype.slice.call(arguments)]);
+    };
+  }
+};
+
+declare class Proxy {
+  constructor (state: any, handler: any);
+}
+
+function makeMockHandler (log) : V8NativeDecoder.IDecodeHandler {
+  var handler = new MockHandlerProxyHandler(log);
+  var result = new Proxy({}, handler);
+  return <V8NativeDecoder.IDecodeHandler>result;
+};
+
+function makeReader (bytes: Array<byte>) {
+  var buf = new Uint8Array(bytes);
+  return new Stream.ValueReader(buf);
+}
--- test/tests.ts
+++ test/tests.ts
@@ -1,40 +1,9 @@
 ///<reference path="../third_party/qunit/qunit.d.ts"/>
 ///<reference path="../third_party/stream.ts"/>
 ///<reference path="../decoder.ts"/>
+///<reference path="testUtil.ts"/>
 
 QUnit.module("tests");
-
-type CallRecord = [string, Array<any>];
-
-class MockHandlerProxyHandler {
-  log : Array<CallRecord>;
-
-  constructor (log: Array<CallRecord>) {
-    this.log = log;
-  }
-
-  get (state, propertyName : string, receiver) {
-    var self = this;
-    return function mockedMethod () {
-      self.log.push([propertyName, Array.prototype.slice.call(arguments)]);
-    };
-  }
-};
-
-declare class Proxy {
-  constructor (state: any, handler: any);
-}
-
-function makeMockHandler (log) : V8NativeDecoder.IDecodeHandler {
-  var handler = new MockHandlerProxyHandler(log);
-  var result = new Proxy({}, handler);
-  return <V8NativeDecoder.IDecodeHandler>result;
-};
-
-function makeReader (bytes: Array<byte>) {
-  var buf = new Uint8Array(bytes);
-  return new Stream.ValueReader(buf);
-}
 
 test("can decode empty module", function (assert) {
   var log = [];
